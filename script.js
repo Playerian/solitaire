6a4a2efe2950361fb651d474f09de6eebfd73554
Liwei Huang <aa583045912@gmail.com>
2018-07-04 16:10:06 +0000

    sutff

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
         this.row = 0;
         this.column = 0;
         this.inTrash = function(){
-            if (trash.includes(this.name)){
+            if (trash.includes(this.name) || (reveal.includes(this.name))){
                 return true;
             } else {
                 return false;
@@ -40,6 +40,29 @@ $(document).ready(function(){
         this.inFound = false;
         this.foundZ = 0;
         this.show = false;
+        //check if another card is different color
+        this.isDiffColor = function(card){
+            //if black
+            if (this.suit === 1 || this.suit === 3){
+                if (card.suit === 2 || card.suit === 4){
+                    return true;
+                }
+            }
+            //if red
+            if (this.suit === 2 || this.suit === 4){
+                if (card.suit === 1 || card.suit === 3){
+                    return true;
+                }
+            }
+            return false;
+        };
+        //check if another card is 1 number higher
+        this.isOneLower = function(card){
+            if (this.number + 1 === card.number){
+                return true;
+            }
+            return false;
+        };
     }
     
     function getCard(num, suit){
@@ -55,6 +78,16 @@ $(document).ready(function(){
             }
         }
         return undefined;
+    }
+    
+    function columnHeight(columnNo){
+        var count = 0;
+        for (var i = 1; i <= 52; i++){
+            if (cards[i].column === columnNo){
+                count ++;
+            }
+        }
+        return count;
     }
     
     //card declare
@@ -91,8 +124,33 @@ $(document).ready(function(){
         trash.push(card);
     }
     
+    //preloading
+    function preload(){
+        for (var i = 1; i <= 52; i ++){
+            var element = $("<img>").attr("class","preload");
+            element.attr("src", "cards/" + card.number + "_" + card.suit + ".png");
+            $("body").append(element);
+        }
+        $(".preload").hide();
+    }
+    preload();
+    
     //rendering
-    var render = function (){
+    function render(){
+        //Any necessary changes relative to cards
+        //if the bottommost card in a column is facedown, then flip it up
+        for (var i = 1; i <= 7; i ++){
+            for (var i2 = 1; i2 <= 17; i2 ++){
+                var card = getCardPos(i, i2);
+                if (card !== undefined){
+                    if (card.show === false && getCardPos(i, i2 + 1) === undefined){
+                        card.show = true;
+                    }
+                }
+            }
+        }
+        
+        //real rendering process
         //remove all cards from view
         $(".card").remove();
         //render in field
@@ -118,15 +176,13 @@ $(document).ready(function(){
             }
         }
         //render in trash
-        for (var i = 1; i <= 52; i ++){
-            if (cards[i].inTrash()){
-                var element = $("<img>").attr("class","card");
-                element.attr("id", i);
-                element.addClass("trash");
-                element.addClass("unclickable");
-                element.attr("src", "cards/back.png");
-                $("#trash").append(element);
-            }
+        for (var i = 0; i < trash.length; i ++){
+            var element = $("<img>").attr("class","card");
+            element.attr("id", cards[trash[i]]);
+            element.addClass("trash");
+            element.addClass("unclickable");
+            element.attr("src", "cards/back.png");
+            $("#trash").append(element);
         }
         //render in reveal
         for (var i = 0; i < reveal.length; i ++){
@@ -147,7 +203,7 @@ $(document).ready(function(){
         
         //add click handlers onto all the cards
         cardClickF();
-    };
+    }
     
     render();
     
@@ -197,22 +253,115 @@ $(document).ready(function(){
         $(".card").click(function(){
             var card = cards[parseInt($(this).attr("id"))];
             console.log(card);
-            //check if card is face up
-            if (card.show === true){
-                //release a card if holding a card
-                if (holding === true){
-                    //check if releasing onto the right card
-                }
-                //take up a card if holding nothing
-                if (holding === false){
-                    holding = true;
-                    holder = card;
+            function mainDish(){
+                //check if card is face up
+                if (card.show === true){
+                    //release a card if holding a card
+                    if (holding === true){
+                        //check if holding the same card
+                        if (card === holder){
+                            //if so remove holding status
+                            holder = undefined;
+                            holding = false;
+                            return;
+                        }
+                        //check if releasing onto the right card
+                        //check if clicked card is not on trash and foundation
+                        if (card.inTrash() === false && card.inFound === false){
+                            //then check if able to move
+                            if (holder.isDiffColor(card) && holder.isOneLower(card)){
+                                //if holder is on the field
+                                console.log(holder);
+                                console.log(card);
+                                console.log(holder.inTrash());
+                                if (holder.inTrash() === false){
+                                    //if so then move the the holder and cards under holder under the card
+                                    //put all cards under the holder into a list
+                                    var list = [];
+                                    for (var i = holder.row; i <= 17; i ++){
+                                        var cardBelow = getCardPos(holder.column, i);
+                                        if (cardBelow !== undefined){
+                                            list.push(cardBelow);
+                                        } else {
+                                            break;
+                                        }
+                                    }
+                                    //after the list has been filled(or not) move them under the (clicked)card
+                                    //store the list length first
+                                    var listLength = list.length;
+                                    //iterate the list
+                                    for (var i = 0; i < listLength; i ++){
+                                        //set column same as the card
+                                        list[i].column = card.column;
+                                        //set row below the card in a fast way
+                                        list[i].row = card.row + i + 1;
+                                    }
+                                } else {
+                                    //if in trash then move directly to field
+                                    holder.column = card.column;
+                                    holder.row = card.row + 1;
+                                    //also remove holder in reveal pile
+                                    duang(reveal, holder.name);
+                                }
+                                //remove holding status
+                                holding = false;
+                                holder = undefined;
+                                //and that's it
+                                return;
+                            }
+                        }
+                    }
+                    //take up a card if holding nothing
+                    if (holding === false){
+                        holding = true;
+                        holder = card;
+                        return;
+                    }
                 }
             }
+            //just add a function to use return
+            mainDish();
             
             render();
         });
     }
+    
+    //move card to empty space (Kings only btw)
+    $(".bottom").click(function(){
+        console.log(cards);
+        var column = $(this).attr("id")[1];
+        console.log(column);
+        console.log("column height: "+columnHeight(column));
+        //check if holding a card and is a king
+        if (holding === true){
+            if (holder.number === 13){
+                //check if column is empty
+                console.log("column height: "+columnHeight(column));
+                if (columnHeight(column) === 0){
+                    //if it's empty then move the whole thing under it
+                    //gathering list(done by c & p)
+                    var list = [];
+                    for (var i = holder.row; i <= 17; i ++){
+                        var cardBelow = getCardPos(holder.column, i);
+                        if (cardBelow !== undefined){
+                            list.push(cardBelow);
+                        } else {
+                            break;
+                        }
+                    }
+                    //move them under the column
+                    for (var i = 0; i < list.length; i ++){
+                        list[i].column = parseInt(column);
+                        list[i].row = i + 1;
+                    }
+                    //remove holding status
+                    holder = undefined;
+                    holding = false;
+                    render();
+                }
+            }
+        }
+    });
     
     
     
